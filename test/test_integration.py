bda39c715a9029b2e96d046940611e7b192381e3
bojiang <bojiang_@outlook.com>
2024-03-05 02:38:53 +0800

    clean

--- test/test_integration.py
+++ test/test_integration.py
@@ -5,18 +5,11 @@ import bentoml
 
 
 def test_iris():
-    with subprocess.Popen(
-        ["bentoml", "serve", "-p", "50001"],
-    ) as server_proc:
-        print("Waiting for server to be ready")
-        time.sleep(8)
-
-        with bentoml.SyncHTTPClient("http://localhost:50001") as client:
-            print("Begin testing")
-            result = client.classify(
-                input_series=[[5.1, 3.5, 1.4, 0.2]],
-            )
-            assert result == [0]
-
-        print("Waiting for server to shutdown")
-        server_proc.terminate()
+    with subprocess.Popen(["bentoml", "serve", "-p", "50001"]) as server_proc:
+        try:
+            time.sleep(8)  # wait for server to start
+            with bentoml.SyncHTTPClient("http://localhost:50001") as client:
+                # Test predict
+                assert client.classify(input_series=[[5.1, 3.5, 1.4, 0.2]]) == [0]
+        finally:
+            server_proc.terminate()
